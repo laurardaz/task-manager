ac6273a03a5c658b1d42facc1078ee6750fc6544
your_github_username <your_github_email>
2023-01-23 21:49:36 +0400

    bug in loaddata, visible only 2

--- my_app/task_manager/main.py
+++ my_app/task_manager/main.py
@@ -38,15 +38,24 @@ class MainWindow(QMainWindow):
         self.ui.tasksTableWidget.setColumnWidth(3, 100)
         self.ui.tasksTableWidget.setColumnWidth(4, 100)
         self.ui.tasksTableWidget.setColumnWidth(5, 100)
-        self.ui.tasksTableWidget.setColumnWidth(0, 100)
-        self.ui.tasksTableWidget.setColumnWidth(0, 150)
+        self.ui.tasksTableWidget.setColumnWidth(6, 100)
+        self.ui.tasksTableWidget.setColumnWidth(7, 150)
         self.loaddata()
 
     def loaddata(self):
-        tasks=[{"название":"Сходить в магазин","описание":"купить еду","проект":"Домашний","приоритет":"Средний",
-                "начало":"12.01.2023","окончание":"13.01.2023","статус":"нет","действие":"нет"},
+        tasks=[{"название":"Сходить в магазин", "описание": "купить еду", "проект": "Домашний","приоритет": "Средний",
+                "начало":"12.01.2023","окончание":"13.01.2023","статус":"В процессе","действие":"нет"},
                {"название": "Доделать модель", "описание": "покрыть лаком", "проект": "Хобби", "приоритет": "Низкий",
-                "начало": "09.01.2023", "окончание": "15.01.2023", "статус": "нет", "действие": "нет"}
+                "начало": "09.01.2023", "окончание": "15.01.2023", "статус": "В процессе", "действие": "нет"},
+               {"название": "Картина", "описание": "купить кисти", "проект": "Хобби", "приоритет": "Низкий",
+                "начало": "17.01.2023", "окончание": "18.01.2023", "статус": "В процессе", "действие": "нет"},
+               {"название": "Высадить цветы", "описание": "купить семена", "проект": "Нет", "приоритет": "Средний",
+               "начало": "01.03.2023", "окончание": "04.03.2023", "статус": "В процессе", "действие": "нет"},
+               {"название": "Прослушать семинар", "описание": "'https://www.youtube.com/watch?v=hP'", "проект": "Нет",
+               "приоритет": "Средний", "начало": "10.02.2023", "окончание": "15.02.2023", "статус": "В процессе",
+               "действие": "нет"},
+               {"название": "Дочитать книги", "описание": "'Прыжок', 'Лето волонтера'", "проект": "Чтение", "приоритет":
+               "Низкий", "начало": "09.01.2023", "окончание": "20.01.2023", "статус": "В процессе", "действие": "нет"}
                ]
         row = 0
         self.ui.tasksTableWidget.setRowCount(len(tasks))
@@ -57,7 +66,10 @@ class MainWindow(QMainWindow):
             self.ui.tasksTableWidget.setItem(row, 3, QtWidgets.QTableWidgetItem(task["приоритет"]))
             self.ui.tasksTableWidget.setItem(row, 4, QtWidgets.QTableWidgetItem(str(task["начало"])))
             self.ui.tasksTableWidget.setItem(row, 5, QtWidgets.QTableWidgetItem(str(task["окончание"])))
-            row =+1
+            self.ui.tasksTableWidget.setItem(row, 6, QtWidgets.QTableWidgetItem(task["статус"]))
+            row =+ 1
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
